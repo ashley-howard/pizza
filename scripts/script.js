0d2328bf2f57f0232ece0e70c450e067f5b63024
Ashley Howard <me@ashley.how>
2020-07-27 11:23:40 +0100

    fix js, add formula, update scss

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,11 @@
-// (Math.PI * QUANTITY)*Math.pow(INCHES/2, 2)
+// Equation: (Math.PI * QUANTITY)*Math.pow(INCHES/2, 2)
 
 const quantity1 = document.getElementById('quantity-1')
 const quantity2 = document.getElementById('quantity-2')
 const measurement1 = document.getElementById('measurement-1')
 const measurement2 = document.getElementById('measurement-2')
 const answer = document.getElementById('answer')
-
-// quantity + measurement
-// function myPizza(q, m) {
-//     console.log((Math.PI * q) * Math.pow(m / 2, 2))
-//     console.log('trunc: ' + Math.trunc((Math.PI * q) * Math.pow(m / 2, 2)) + " in\xB2")
-// }
+const formula = document.getElementById('formula')
 
 function pizzaCalc(q1, m1, q2, m2) {
     q1 = +quantity1.value;
@@ -18,37 +13,30 @@ function pizzaCalc(q1, m1, q2, m2) {
     m1 = +measurement1.value;
     m2 = +measurement2.value;
 
-    // console.log(q1, q2, m1, m2)
+    if (q1, q2, m1, m2) {
+        pizza1 = (Math.PI * q1) * Math.pow(m1 / 2, 2)
+        pizza2 = (Math.PI * q2) * Math.pow(m2 / 2, 2)
 
-    // myPizza(q1, m1)
-    // myPizza(q2, m2)
+        if (pizza1 > pizza2) {
+            answer.innerText = `Yes, by ${Math.round(pizza1 - pizza2)} inches`
+        } else if (pizza1 == pizza2) {
+            answer.innerText = `No, they're the same`
+        } else {
+            answer.innerText = `No, by ${Math.round(pizza2 - pizza1)} inches`
+        }
 
-    pizza1 = (Math.PI * q1) * Math.pow(m1 / 2, 2)
-    pizza2 = (Math.PI * q2) * Math.pow(m2 / 2, 2)
+        document.getElementById('formula-title').style.display = 'block'
+        formula.innerText = `Area 1 = ${q1}\u03C0(${m1}/2)\xB2 = ${Math.round(pizza1)} in\xB2 \n Area 2 = ${q2}\u03C0(${m2}/2)\xB2 = ${Math.round(pizza2)} in\xB2`
 
-    // console.log((Math.PI * q1) * Math.pow(m1 / 2, 2) > (Math.PI * q2) * Math.pow(m2 / 2, 2))
-    // console.log("(by " + ((Math.PI * q1) * Math.pow(m1 / 2, 2) - (Math.PI * q2) * Math.pow(m2 / 2, 2)) + ' inches.')
-    // console.log(`(by ${Math.trunc((Math.PI * q1) * Math.pow(m1 / 2, 2) - (Math.PI * q2) * Math.pow(m2 / 2, 2))} inches.)`)
-
-
-
-    if (pizza1 > pizza2) {
-        answer.innerText = `Yes, by ${Math.trunc(pizza1 - pizza2)} inches`
-    } else if (pizza1 == pizza2) {
-        answer.innerText = `No, they're the same`
     } else {
-        answer.innerText = `No, by ${Math.trunc(pizza2 - pizza1)} inches`
+        answer.innerText = `Please fill out all inputs`
     }
 
+    // writing formula
+    // \u03C0
 
+    // if (q1 > 1){
 
-
-
-    // console.log(pizza1 > pizza2)
-    // console.log((Math.PI * q1) * Math.pow(m1 / 2, 2))
-    // console.log('trunc: ' + Math.trunc((Math.PI * q1) * Math.pow(m1 / 2, 2)) + " in\xB2")
-
-    // console.log((Math.PI * q2) * Math.pow(m2 / 2, 2))
-    // console.log('trunc: ' + Math.trunc((Math.PI * q2) * Math.pow(m2 / 2, 2)) + " in\xB2")
+    // }
 
 }
