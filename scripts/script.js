efb647ee90d1f68a97b9c9e9bebdc556dad028f9
Ashley Howard <me@ashley.how>
2020-07-27 18:55:33 +0100

    fix mobile display, add reset, update colors

--- scripts/script.js
+++ scripts/script.js
@@ -12,22 +12,24 @@ function pizzaCalc(q1, m1, q2, m2) {
     q2 = +quantity2.value;
     m1 = +measurement1.value;
     m2 = +measurement2.value;
+    var diff = 0;
 
     if (q1, q2, m1, m2) {
         pizza1 = (Math.PI * q1) * Math.pow(m1 / 2, 2)
         pizza2 = (Math.PI * q2) * Math.pow(m2 / 2, 2)
 
         if (pizza1 > pizza2) {
-            answer.innerText = `Yes, by ${Math.round(pizza1 - pizza2)} inches`
+            answer.innerText = `Yes`
+            diff = Math.round(pizza1 - pizza2)
         } else if (pizza1 == pizza2) {
-            answer.innerText = `No, they're the same`
+            answer.innerText = `They're the same`
         } else {
-            answer.innerText = `No, by ${Math.round(pizza2 - pizza1)} inches`
+            answer.innerText = `No`
+            diff = Math.round(pizza2 - pizza1)
         }
 
-        document.getElementById('formula-title').style.display = 'block'
-        document.getElementById('formula').style.display = 'block'
-        formula.innerText = `Area 1 = ${q1}\u03C0(${m1}/2)\xB2 = ${Math.round(pizza1)} in\xB2 \n Area 2 = ${q2}\u03C0(${m2}/2)\xB2 = ${Math.round(pizza2)} in\xB2`
+        document.getElementById('formula-container').style.display = 'block'
+        formula.innerText = `Area 1 = ${q1}\u03C0(${m1}/2)\xB2 = ${Math.round(pizza1)} in\xB2 \n Area 2 = ${q2}\u03C0(${m2}/2)\xB2 = ${Math.round(pizza2)} in\xB2 \n\n Difference: ${diff} in\xB2`
 
     } else {
         answer.innerText = `Please fill out all inputs`
@@ -35,28 +37,23 @@ function pizzaCalc(q1, m1, q2, m2) {
 }
 
 function updateGrammar() {
-    q1 = +quantity1.value;
-    q2 = +quantity2.value;
-    m1 = +measurement1.value;
-    m2 = +measurement2.value;
-
-    if (q1 > 1) {
-        document.getElementById('grammar-1').innerText = 'Are'
-        document.getElementById('grammar-2').innerText = ' inch pizzas,'
+    if (+quantity1.value > 1) {
+        document.getElementById('w-1').innerText = 'Are'
+        document.getElementById('w-2').innerText = ' inch pizzas,'
 
     } else {
-        document.getElementById('grammar-1').innerText = 'Is'
-        document.getElementById('grammar-2').innerText = ' inch pizza,'
+        document.getElementById('w-1').innerText = 'Is'
+        document.getElementById('w-2').innerText = ' inch pizza,'
     }
-    if (q2 > 1) {
-        document.getElementById('grammar-3').innerText = 'inch pizzas?'
+    if (+quantity2.value > 1) {
+        document.getElementById('w-3').innerText = 'inch pizzas?'
 
     } else {
-        document.getElementById('grammar-3').innerText = 'inch pizza?'
+        document.getElementById('w-3').innerText = 'inch pizza?'
     }
 }
 
 function reset() {
     quantity1.value = quantity2.value = measurement1.value = measurement2.value = answer.innerText = '';
-    document.getElementById('formula-title').style.display = document.getElementById('formula').style.display = 'none'
+    document.getElementById('formula-container').style.display = 'none'
 }
